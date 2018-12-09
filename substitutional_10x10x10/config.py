4b60643397d20d1d706c8ff8345628cde6ca40b0
atan14 <tanaikrui@hotmail.com>
2018-12-08 19:45:05 -0600

    Changes to be committed: 	new file:   interstitial_10x10x10/plotting.py 	new file:   substitutional_10x10x10/Diffusion3DTest.py 	new file:   substitutional_10x10x10/config.py 	new file:   substitutional_10x10x10/processes.py

---
+++ substitutional_10x10x10/config.py
@@ -0,0 +1,54 @@
+# Copyright (c)  2014  Mikael Leetmaa
+#
+# This file is part of the KMCLib project distributed under the terms of the
+# GNU General Public License version 3, see <http://www.gnu.org/licenses/>.
+#
+
+from KMCLib import *
+import random
+
+# -----------------------------------------------------------------------------
+# Unit cell
+
+cell_vectors = [[   5.762860e+00,   0.000000e+00,   0.000000e+00],
+                [   0.000000e+00,   5.762860e+00,   0.000000e+00],
+                [   0.000000e+00,   0.000000e+00,   5.762860e+00]]
+
+basis_points = [[0.0, 0.0, 0.0], #0
+                [0.25, 0.75, 0.75], #1
+                [0.5, 0.0, 0.5],#2
+                [0.0, 0.5, 0.5],#3
+                [0.5, 0.5, 0.0], #4
+                [0.75, 0.25, 0.75],#5
+                [0.75, 0.75, 0.25], #6
+                [0.25, 0.25, 0.25]] #7
+
+unit_cell = KMCUnitCell(
+    cell_vectors=cell_vectors,
+    basis_points=basis_points)
+
+# -----------------------------------------------------------------------------
+# Lattice
+# Note that we run with periodicity in y and z but not in x.
+lattice = KMCLattice(
+    unit_cell=unit_cell,
+    repetitions=(10,10,10),
+    periodic=(True, True, True))
+
+# -----------------------------------------------------------------------------
+# Configuration
+
+num_atoms = 10*10*10*len(basis_points)
+
+types = ['Ge']*num_atoms
+
+for i in range(20):
+    types[random.randint(1, num_atoms)] = 'V'
+types[random.randint(1, num_atoms)] = 'B'
+
+possible_types = ['Ge', 'B', 'V']
+
+configuration = KMCConfiguration(
+    lattice=lattice,
+    types=types,
+    possible_types=possible_types)
